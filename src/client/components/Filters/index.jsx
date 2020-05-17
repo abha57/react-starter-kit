ef0d41e741d8a7f3d4273531ee1ba43b5b548dcd
Apaar <you@example.com>
2020-05-17 07:24:57 +0530

    changes

--- src/client/components/Filters/index.jsx
+++ src/client/components/Filters/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
-const Filters = (props) => {
-    const { inputProps, filterProps } = props;
-return (
+const Filters = props => {
+  const { inputProps, filterProps } = props;
+  return (
     <React.Fragment>
-    <div>
-        Search By name: 
-        <input type="text" value={inputProps.value} name={inputProps.name} {...inputProps.textProps} />
-    </div>
-    <div> Sort By Rating: 
-
-    </div>
-    {/* <div>
+      <div>
+        Search By name:
+        <input
+          type="text"
+          value={inputProps.value}
+          name={inputProps.name}
+          {...inputProps.textProps}
+        />
+      </div>
+      <div> Sort By Rating:</div>
+      {/* <div>
         <select name="cuisines" id="cuisines" {...filterProps.selectProps} >
         {filterProps.options.map((option) => <option value={option.value}>{option.label}</option> )}
         </select>
     </div> */}
     </React.Fragment>
-)
+  );
 };
 
 export default Filters;
