8aa7b06330710c56d6c51da5fda7ee4445d578e3
Apaar <you@example.com>
2020-06-13 14:40:00 +0530

    changes

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import Header from './components/Header';
 
 const App = () => {
-    return (
-        <React.Fragment>
-           <div className='container'>
-            <Header />
-           </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      herlooew
+      <div className="container">
+        <Header />
+      </div>
+    </React.Fragment>
+  );
 };
-
 
 export default App;
