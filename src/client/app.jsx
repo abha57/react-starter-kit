5380a65f74f2bcebd0b768e0d57e016302d8f28c
Apaar <you@example.com>
2020-06-13 18:18:00 +0530

    changes

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Header from './components/Header';
 
+import './styles.scss';
+
 const App = () => {
   return (
     <React.Fragment>
-      herlooew
       <div className="container">
         <Header />
+        <section className="cover cover-post cover-post-background">
+          <div className="cover-background"></div>
+          <div className="cover-contents">
+            <section>
+              <h1 className="post-title">APAAR BHATNAGAR</h1>
+              <h3 className="post-content">
+                I am a professional Full Stack web Developer with sense of
+                translating business ideas into technical realm. From designs,
+                to making your ideas moving on your screens, to providing the
+                backend support, I will be there for you.
+              </h3>
+            </section>
+          </div>
+          <div className="cover-contents">
+            <img src="public/babayoda.jpeg" alt="image" />
+          </div>
+        </section>
       </div>
     </React.Fragment>
   );
--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import './style.scss';
 
 const Header = () => {
-    return (
-        <div className='header'>Header</div> 
-    )
-}
+  return (
+    <div className="full-header">
+      <div className="full-header-spacing">
+        <ul>
+          <li>link1</li>
+          <li>link2</li>
+          <li>link3</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Header;
